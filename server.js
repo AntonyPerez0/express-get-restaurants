27580db8cf8d226053f1a688d6171ce84967ddb7
Antony Perez <antonyperez0@yahoo.com>
2024-11-11 09:08:35 -0800

    done

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
-const app = require("./src/app")
+const app = require("./src/app");
 const db = require("./db/connection");
-const port = 3000;
+const port = 3001;
 
-//TODO: Create your GET Request Route Below: 
+//TODO: Create your GET Request Route Below:
+const Restaurant = require("./models/Restaurant");
 
+app.get("/restaurants", async (req, res) => {
+  try {
+    const restaurants = await Restaurant.findAll();
+    res.json(restaurants);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 app.listen(port, () => {
-    db.sync();
-    console.log(`Listening at http://localhost:${port}/restaurants`);
-})
+  db.sync();
+  console.log(`Listening at http://localhost:${port}/restaurants`);
+});
